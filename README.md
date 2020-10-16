173464bdb7db9652a620c127ea35984ad979310e
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-10-16 12:50:11 +0530

    chore: add util functions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # blessed-utils
 Util functions for blessed-cli apps
+
+## getTheme
+Get the theme colors from the config file
+
+## getManifest
+Get the package.json information from the working directory
+
+## runCommand
+Open a terminal widget and run a command inside blessed-cli apps.
---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const neoblessed = require('@blessed/neo-blessed');
+
+function getManifest() {
+  let pkg = {
+    scripts: {},
+    devDependencies: {},
+    dependencies: {},
+  };
+
+  try {
+    pkg = require(process.cwd() + '/package.json');
+  } catch (e) {
+    // throw error
+  }
+
+  return pkg;
+};
+
+function getTheme(configFile) {
+  const configPath =
+    process.env.OS === 'Windows_NT'
+      ? path.resolve(process.env.APPDATA, `${configFile}`)
+      : path.resolve(process.env.HOME, `.config/${configFile}`);
+
+  let config = { theme: 'Dracula' };
+  try {
+    config = require(configPath);
+  } catch (e) {
+    // Write config file
+    fs.writeFile(configPath, JSON.stringify(config, null, 2), (err) => {
+      if (err) throw err;
+    });
+  }
+
+  const colors = require(`blessed-themes/themes/${config.theme}`);
+  const theme = require('../styles')(colors.colors);
+  return theme;
+};
+
+
+'use strict';
+
+
+module.exports = function runCommand(screen, cmd, configFile) {
+  const theme = getTheme(configFile);
+  const {
+    terminal: { border, style },
+  } = theme;
+  const terminal = neoblessed.terminal({
+    parent: screen,
+    top: 'center',
+    left: 'center',
+    width: '50%',
+    height: '50%',
+    border,
+    style,
+    label: cmd,
+    fullUnicode: true,
+    screenKeys: false,
+    cwd: process.env.PWD,
+  });
+  screen.append(terminal);
+  screen.render();
+  terminal.focus();
+
+  terminal.key('escape', function () {
+    terminal.detach();
+  });
+
+  terminal.pty.write(`${cmd}\r\n`);
+};
+
+module.exports = {
+  getManifest,
+  getTheme,
+  runCommand,
+};
